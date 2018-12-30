a0ca250a37b9b4ec4ae9b4745607afefc048da66
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-12-30 19:31:58 +1100

    Props and props.children for Comment and Approval Card

--- comment-component/src/index.js
+++ comment-component/src/index.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
+
 const App = () => {
   return (
-    <div className="ui container comments">
-      <CommentDetail
-        author="Sharad"
-        timeAgo="Today at 5:34 PM"
-        comment="Tero Bau!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Hari"
-        timeAgo="Yesterday at 2:00 PM"
-        comment="JPT xa khate!!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Ram"
-        timeAgo="Today at 4:55 PM"
-        comment="Thikai xa nita!"
-        avatar={faker.image.avatar()}
-      />
+    <div style={{ margin: '50px' }} className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4 style={{ color: 'red' }}> Warning</h4>
+          <p>Are you Sure?</p>
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sharad"
+          timeAgo="Today at 5:34 PM"
+          comment="Tero Bau!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Hari"
+          timeAgo="Yesterday at 2:00 PM"
+          comment="JPT xa khate!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Ram"
+          timeAgo="Today at 4:55 PM"
+          comment="Thikai xa nita!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
